e94ed7c95793b14785b74890ee5a6dffa14cfca9
Nadeem Shakoor <raonadeemshakoor2@gmail.com>
2026-05-23 18:25:01 +0500

    Add Dockerfile for Hugging Face

--- app.py
+++ app.py
@@ -70,3 +70,7 @@ if __name__ == '__main__':
     print("  Running at: http://localhost:5000")
     print("=" * 50)
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+import os
+port = int(os.environ.get("PORT", 7860))
